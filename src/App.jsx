6d4f4f3ab8a9ec01b12bda6e78a75815e1dda15e
NisargaCode <nisargakn726@gmail.com>
2025-11-25 10:38:59 +0530

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <div className="text-xl font-bold text-blue-600">Tailwind is Working 🎉</div>
+
+      
+    </>
+  )
+}
+
+export default App
